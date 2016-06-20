13d18f7c779781d7c78bcaf6befe1e328c641a59
Josh Rezende <joserenato2@gmail.com>
2016-06-20 14:48:20 -0300

    move api credentials to env file

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ const webpackMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 const config = require('./webpack.config.js');
 const Twitter = require('twitter');
+require('dotenv').config();
 
 const twitterClient = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRE,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
