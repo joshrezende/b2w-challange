fbd783058db857b0b8affae3d16875d2c4047f13
Josh Rezende <joserenato2@gmail.com>
2016-06-20 14:38:10 -0300

    move api credentials to env

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const config = require('./webpack.config.js');
 const Twitter = require('twitter');
 
 const twitterClient = new Twitter({
-  consumer_key: 'XmSFZJxfZ0TVStwHnbT0Zg',
-  consumer_secret: '0YKFvplc836UXG316M6hd86mff5Pp01mbZvPTzIiY',
-  access_token_key: '14172217-G8kdnQ7cFAYeGZ0oUIBPgIyiK0p0lXpeGiz28TdYq',
-  access_token_secret: 'yrSMOWtGb9QYzGY3I8SFbxk7AwRJKIabFwUam6Ihq9w2s'
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRE,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 const isDeveloping = process.env.NODE_ENV !== 'production';
