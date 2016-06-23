b47db43ce0b02e993dae296d54d35dea2fcd2d03
Josh Rezende <joserenato2@gmail.com>
2016-06-22 23:51:34 -0300

    create sub elements scss files for header, topbar and made logic for fixed header

--- src/js/components/Topbar.js
+++ src/js/components/Topbar.js
@@ -8,16 +8,16 @@ export default class Topbar extends React.Component {
           <h1 className="logo">Twitter</h1>
           <ul className="topbar-menu">
             <li className="item">
-              <a href="#">Home</a>
+              <a href="#"><span class="icon icon-home"></span>Home</a>
             </li>
             <li className="item">
-              <a href="#">Moments</a>
+              <a href="#"><span class="icon icon-thunder"></span>Moments</a>
             </li>
             <li className="item">
-              <a href="#">Notifications</a>
+              <a href="#"><span class="icon icon-bell"></span>Notifications</a>
             </li>
             <li className="item">
-              <a href="#">Messages</a>
+              <a href="#"><span class="icon icon-message"></span>Messages</a>
             </li>
           </ul>
         </div>
